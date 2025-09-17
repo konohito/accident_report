174c71b9f901d992ac537e40d21ab0ca5bd58f46
Jumps710 <nonattonolife@gmail.com>
2025-09-17 19:20:23 +0900

    Update accident-report.js

--- accident-report.js
+++ accident-report.js
@@ -3,7 +3,9 @@
 // 設定
 const config = {
     woffId: 'k7_SVZ1p8vy45jQkIRvOUw', // 本番環境のWOFF ID
-    gasUrl: 'https://script.google.com/macros/s/AKfycbxD9kzCqRreieyw1WDNADsaw_zLsmYGB6pTiue-5Vuw0-2KcViZ4MNM_TtQkeASIkN7OA/exec',
+   // gasUrl: 'https://script.google.com/macros/s/AKfycbxD9kzCqRreieyw1WDNADsaw_zLsmYGB6pTiue-5Vuw0-2KcViZ4MNM_TtQkeASIkN7OA/exec', // Cruto様本番環境
+    gasUrl: 'https://script.google.com/macros/s/AKfycby5fRaVu5vISA3dvflBAaYXtWtBGXRyWt9HpWYlAiWbqqHzyBxSAt6vpWn6NuWFk8Gj/exec', // 村松テスト
+    
     googleMapsApiKey: 'AIzaSyCdhA4t8flujiYex2OddJCkFv4u6nWvi9w' // Google Maps Geocoding API
 };
 
